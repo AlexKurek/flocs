9f6dc03054c86de772211cafdd1b103cd41704d8
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-02-14 08:49:39 +0000

    Make parameter updates more clear to the user

--- runners/create_ms_list.py
+++ runners/create_ms_list.py
@@ -1350,9 +1350,9 @@ def parse_arguments_linc(args: dict):
         print("Generating LINC Target config")
         if args["output_fullres_data"]:
             print("Full-resolution data requested, updating defaults to:")
-            print(f"{args['avg_timeresolution']} -> 1")
-            print(f"{args['avg_freqresolution']} -> 12.21kHz")
-            print(f"{args['filter_baselines']} -> *&")
+            print(f"avg_timeresolution: {args['avg_timeresolution']} -> 1")
+            print(f"avg_freqresolution: {args['avg_freqresolution']} -> 12.21kHz")
+            print(f"filter_baselines: {args['filter_baselines']} -> *&")
             args["avg_timeresolution"] = 1
             args["avg_freqresolution"] = "12.21kHz"
             args["filter_baselines"] = "*&"
